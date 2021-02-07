79497dff552b4cb09427af98588af6a9a6baba2f
pinkfloydfan <jxchen98@gmail.com>
2021-02-07 16:58:22 +0000

    disabled CORS for testing purposes

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,82 @@
 #include <WiFi.h>
-#include <WebSocketServer.h>
- 
-WiFiServer server(80);
-WebSocketServer webSocketServer;
- 
-const char *ssid = "MyESP32AP";
-const char *password = "testpassword";
- 
-void setup() {
- 
-  Serial.begin(115200);
- 
-  delay(4000);
- 
-  WiFi.softAP(ssid, password);
-  Serial.println(WiFi.softAPIP());
- 
-  server.begin();
-  delay(100);
-}
- 
-void loop() {
- 
-  WiFiClient client = server.available();
- 
-  if (client.connected() && webSocketServer.handshake(client)) {
- 
-    String data;      
- 
-    while (client.connected()) {
- 
-      data = webSocketServer.getData();
- 
-      if (data.length() > 0) {
-         Serial.println(data);
-         webSocketServer.sendData(data);
+#include <WebSocketsServer.h>
+
+// Constants
+const char* ssid = "COVID-19 5G Tower";
+const char* password = "password";
+
+IPAddress local_IP(192,168,4,22);
+IPAddress gateway(192,168,4,9);
+IPAddress subnet(255,255,255,0);
+
+// Globals 
+// also we've disabled CORS access control...
+WebSocketsServer webSocket = WebSocketsServer(80, "*");
+
+// Called when receiving any WebSocket message
+void onWebSocketEvent(uint8_t num,
+                      WStype_t type,
+                      uint8_t * payload,
+                      size_t length) {
+
+  // Figure out the type of WebSocket event
+  switch(type) {
+
+    // Client has disconnected
+    case WStype_DISCONNECTED:
+      Serial.printf("[%u] Disconnected!\n", num);
+      break;
+
+    // New client has connected
+    case WStype_CONNECTED:
+      {
+        IPAddress ip = webSocket.remoteIP(num);
+        Serial.printf("[%u] Connection from ", num);
+        Serial.println(ip.toString());
       }
- 
-      delay(10); // Delay needed for receiving the data correctly
-   }
- 
-   Serial.println("The client disconnected");
-   delay(100);
+      break;
+
+    // Echo text message back to client
+    case WStype_TEXT:
+      Serial.printf("[%u] Text: %s\n", num, payload);
+      webSocket.sendTXT(num, payload);
+      break;
+
+    // For everything else: do nothing
+    case WStype_BIN:
+    case WStype_ERROR:
+    case WStype_FRAGMENT_TEXT_START:
+    case WStype_FRAGMENT_BIN_START:
+    case WStype_FRAGMENT:
+    case WStype_FRAGMENT_FIN:
+    default:
+      break;
   }
- 
-  delay(100);
+}
+
+void setup() {
+
+  // Start Serial port
+  Serial.begin(115200);
+
+  Serial.print("Setting soft-AP configuration ... ");
+  Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
+
+  // create access point 
+  Serial.println("Creating AP");
+  WiFi.softAP(ssid, password, 1, false, 4);
+  // Print our IP address
+  Serial.println("Connected!");
+  Serial.print("My IP address: ");
+  Serial.println(WiFi.softAPIP());
+
+  // Start WebSocket server and assign callback
+  webSocket.begin();
+  webSocket.onEvent(onWebSocketEvent);
+}
+
+void loop() {
+
+  // Look for and handle WebSocket data
+  webSocket.loop();
 }
