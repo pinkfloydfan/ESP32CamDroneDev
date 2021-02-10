8ade140721dd3a72eab3e962877bb2e2be6152e0
pinkfloydfan <jxchen98@gmail.com>
2021-02-10 21:32:02 +0000

    attempted to implement SBUS over UART1. Experiment has not proven to be successful so far.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,98 @@
 #include <WiFi.h>
 #include <WebSocketsServer.h>
+#include <HardwareSerial.h>
 
 // Constants
+
+//Allocate GPIO12 to sbus tx
+//#define E_UART_TXD (GPIO_NUM_12)
+
+HardwareSerial Serial_1(1);
+//#define SERIAL1_TX 12
+
+//WiFi AP
 const char* ssid = "COVID-19 5G Tower";
 const char* password = "password";
+
+int uart_num; 
 
 IPAddress local_IP(192,168,4,22);
 IPAddress gateway(192,168,4,9);
 IPAddress subnet(255,255,255,0);
 
+//SBUS - Creds to Pawel S: https://quadmeup.com/generate-s-bus-with-arduino-in-a-simple-way/
+
+#define RC_CHANNEL_MIN 990
+#define RC_CHANNEL_MAX 2010
+
+#define SBUS_MIN_OFFSET 173
+#define SBUS_MID_OFFSET 992
+#define SBUS_MAX_OFFSET 1811
+#define SBUS_CHANNEL_NUMBER 16
+#define SBUS_PACKET_LENGTH 25
+#define SBUS_FRAME_HEADER 0x0f
+#define SBUS_FRAME_FOOTER 0x00
+#define SBUS_FRAME_FOOTER_V2 0x04
+#define SBUS_STATE_FAILSAFE 0x08
+#define SBUS_STATE_SIGNALLOSS 0x04
+#define SBUS_UPDATE_RATE 15 //ms
+
+void sbusPreparePacket(uint8_t packet[], int channels[], bool isSignalLoss, bool isFailsafe){
+
+    static int output[SBUS_CHANNEL_NUMBER] = {0};
+
+    /*
+     * Map 1000-2000 with middle at 1500 chanel values to
+     * 173-1811 with middle at 992 S.BUS protocol requires
+     */
+    for (uint8_t i = 0; i < SBUS_CHANNEL_NUMBER; i++) {
+        output[i] = map(channels[i], RC_CHANNEL_MIN, RC_CHANNEL_MAX, SBUS_MIN_OFFSET, SBUS_MAX_OFFSET);
+    }
+
+    uint8_t stateByte = 0x00;
+    if (isSignalLoss) {
+        stateByte |= SBUS_STATE_SIGNALLOSS;
+    }
+    if (isFailsafe) {
+        stateByte |= SBUS_STATE_FAILSAFE;
+    }
+    packet[0] = SBUS_FRAME_HEADER; //Header
+
+    packet[1] = (uint8_t) (output[0] & 0x07FF);
+    packet[2] = (uint8_t) ((output[0] & 0x07FF)>>8 | (output[1] & 0x07FF)<<3);
+    packet[3] = (uint8_t) ((output[1] & 0x07FF)>>5 | (output[2] & 0x07FF)<<6);
+    packet[4] = (uint8_t) ((output[2] & 0x07FF)>>2);
+    packet[5] = (uint8_t) ((output[2] & 0x07FF)>>10 | (output[3] & 0x07FF)<<1);
+    packet[6] = (uint8_t) ((output[3] & 0x07FF)>>7 | (output[4] & 0x07FF)<<4);
+    packet[7] = (uint8_t) ((output[4] & 0x07FF)>>4 | (output[5] & 0x07FF)<<7);
+    packet[8] = (uint8_t) ((output[5] & 0x07FF)>>1);
+    packet[9] = (uint8_t) ((output[5] & 0x07FF)>>9 | (output[6] & 0x07FF)<<2);
+    packet[10] = (uint8_t) ((output[6] & 0x07FF)>>6 | (output[7] & 0x07FF)<<5);
+    packet[11] = (uint8_t) ((output[7] & 0x07FF)>>3);
+    packet[12] = (uint8_t) ((output[8] & 0x07FF));
+    packet[13] = (uint8_t) ((output[8] & 0x07FF)>>8 | (output[9] & 0x07FF)<<3);
+    packet[14] = (uint8_t) ((output[9] & 0x07FF)>>5 | (output[10] & 0x07FF)<<6);  
+    packet[15] = (uint8_t) ((output[10] & 0x07FF)>>2);
+    packet[16] = (uint8_t) ((output[10] & 0x07FF)>>10 | (output[11] & 0x07FF)<<1);
+    packet[17] = (uint8_t) ((output[11] & 0x07FF)>>7 | (output[12] & 0x07FF)<<4);
+    packet[18] = (uint8_t) ((output[12] & 0x07FF)>>4 | (output[13] & 0x07FF)<<7);
+    packet[19] = (uint8_t) ((output[13] & 0x07FF)>>1);
+    packet[20] = (uint8_t) ((output[13] & 0x07FF)>>9 | (output[14] & 0x07FF)<<2);
+    packet[21] = (uint8_t) ((output[14] & 0x07FF)>>6 | (output[15] & 0x07FF)<<5);
+    packet[22] = (uint8_t) ((output[15] & 0x07FF)>>3);
+
+    packet[23] = stateByte; //Flags byte
+    packet[24] = SBUS_FRAME_FOOTER; //Footer
+}
+
+uint8_t sbusPacket[SBUS_PACKET_LENGTH];
+int rcChannels[SBUS_CHANNEL_NUMBER];
+uint32_t sbusTime = 0;
+
+//SBUS end
+
 // Globals 
-// also we've disabled CORS access control...
+// also we've disabled CORS access control with the 2nd argument...
 WebSocketsServer webSocket = WebSocketsServer(80, "*");
 
 // Called when receiving any WebSocket message
@@ -56,8 +138,9 @@ void onWebSocketEvent(uint8_t num,
 
 void setup() {
 
-  // Start Serial port
-  Serial.begin(115200);
+  // Start Serial monitor port
+  //Serial.begin(115200);
+  Serial.begin(100000, SERIAL_8E2); //test 
 
   Serial.print("Setting soft-AP configuration ... ");
   Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
@@ -73,9 +156,40 @@ void setup() {
   // Start WebSocket server and assign callback
   webSocket.begin();
   webSocket.onEvent(onWebSocketEvent);
+
+  //Setup SBUS output
+
+  for (uint8_t i = 0; i < SBUS_CHANNEL_NUMBER; i++) {
+        rcChannels[i] = 1500;
+    }
+
+  //100000 for sbus, 8E2 - 8 bits, even, 2 stop bits
+  Serial1.begin(100000, SERIAL_8E2, 12, 13); 
+
 }
 
 void loop() {
+  
+    uint32_t currentMillis = millis();
+
+    /*
+     * Here you can modify values of rcChannels while keeping it in 1000:2000 range
+     */
+
+    rcChannels[0] = 1500;
+    rcChannels[1] = 1200;
+    rcChannels[2] = 1900;
+
+    if (currentMillis > sbusTime) {
+        sbusPreparePacket(sbusPacket, rcChannels, false, false);
+        //uart_write_bytes(uart_num, rcChannels, SBUS_PACKET_LENGTH)
+        //Serial.print("Writing SBUS... ");
+        Serial1.write(sbusPacket, SBUS_PACKET_LENGTH);
+        Serial.write(sbusPacket, SBUS_PACKET_LENGTH);
+
+
+        sbusTime = currentMillis + SBUS_UPDATE_RATE;
+    }
 
   // Look for and handle WebSocket data
   webSocket.loop();
